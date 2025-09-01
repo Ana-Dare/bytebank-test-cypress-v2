b4251822a9b8045764c65aea706df6cb8ce33478
Ana-Dare <analauradare467@gmail.com>
2025-09-01 07:50:10 -0300

    Teste utilitário para responsividade

--- cypress.config.js
+++ cypress.config.js
@@ -4,4 +4,9 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000'
   },
+  env: {
+    mobileViewPortWidthBreakpoint: 420
+  },
+  viewportWidth: 1200,
+  viewportHeight: 990,
 });
